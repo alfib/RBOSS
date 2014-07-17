4c868e670e8d13fd8c8e89f4ac1413f04133e03e
Bidur <bidurbs@gmail.com>
2014-07-16 21:20:00 -0500

    minor change in searching

--- src/main/java/com/mycompany/rboss/controller/ProductController.java
+++ src/main/java/com/mycompany/rboss/controller/ProductController.java
@@ -107,7 +107,7 @@ public class ProductController {
         List<Product> products = productService.getProductByName(productName);
         int noOfProducts = products.size();
         //if (noOfProducts > 0) {  //searched product found           
-            re.addFlashAttribute("products", products);
+            model.addAttribute("products", products);
             model.addAttribute("noOfProducts", noOfProducts);
             List<Category> allCategories = categoryService.getAll();
             model.addAttribute("categories", allCategories);
